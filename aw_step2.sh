841425b2657335dfbfc6144ad566092955ad1ef8
davidmosen <davidmosen@yahoo.es>
2017-03-28 15:40:13 +0100

    Still working on first QC steps

--- aw_step2.sh
+++ aw_step2.sh
@@ -13,9 +13,15 @@ i=${PBS_ARRAY_INDEX}
 source ${PBS_O_WORKDIR}/aw_params.sh
 
 module load plink
+module load R
+# TODO check that all required R packages are installed, including geneplotter
 
 cd $TMPDIR
 cp ${ZCALLPATH}/n${i}.zcall .
+
+##############
+### PRE-QC ###
+##############
 
 ### preprocessing
 python ${PBS_O_WORKDIR}/convertReportToTPED.py -R n${i}.zcall -O n${i}.tmp
@@ -23,22 +29,23 @@ plink --tfile n${i}.tmp --make-bed --out n${i}.raw
 plink --noweb --bfile n${i}.raw --update-alleles ${STRANDPATH}.update_alleles.txt --make-bed --out n${i}
 ${PBS_O_WORKDIR}/update_build.sh n${i} ${STRANDPATH}-${GENOMEBUILD}.strand n${i}
 
-### sex problems
+### update sex with detected one
 plink --bfile n${i} --check-sex --out n${i}
-grep PROBLEM n${i}.sexcheck > n${i}.sexprobs
-# TODO write to fail-sexcheck-qc.txt
+plink --bfile n${i} --update-sex n${i}.sexcheck 2 --make-bed --out n${i}
+
+##############
+### QC #######
+##############
 
 ### missing data and abnormal heterozygosity rate
 plink --bfile n${i} --missing --out n${i}
 plink --bfile n${i} --het --out n${i}
-# TODO check that all required R packages are installed, including geneplotter
 Rscript ${PBS_O_WORKDIR}/imiss-vs-het_modified.R n${i}
+plink --bfile n${i} --remove n${i}.fail-imisshet-qc.txt --make-bed --out n${i}.clean-inds
 
-### relatedness
-# TODO do this once batches are merged?
-plink --bfile n${i} --indep-pairwise 50 5 0.2 --out n${i}
-plink --bfile n${i} --extract n${i}.prune.in --genome --out n${i}
-Rscript ${PBS_O_WORKDIR}/plot-IBD_modified.R n${i}
-# TODO write to fail-IBD-qc.txt
+### markers with excessive missing data
+#plink --bfile n${i}.clean-inds --missing --out n${i}.clean-inds
+#Rscript ../lmiss-hist_modified.R n1
+plink --bfile n${i}.clean-inds --maf 0.01 --geno 0.05 --hwe 0.00001 --make-bed --out n${i}.clean
 
-find . -type f |grep -vE '(.zcall$|.tmp.|.py$)' |xargs -ILIST cp LIST ${PLINKPATH}
+cp n{i}.clean.* ${PLINKPATH}
