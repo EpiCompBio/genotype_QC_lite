0381d7ec62b69e9b9132fe690012482b2a0cc854
davidmosen <davidmosen@yahoo.es>
2017-03-29 15:24:58 +0100

    Work on marker QC

--- aw_step1.sh
+++ aw_step1.sh
@@ -1,11 +1,16 @@
 module load plink
 module load R
+# TODO check it's installed: devtools::install_github("gabraham/flashpca/flashpcaR")
 
 set -eu
 
 PARAMS=/groupvol/med-bio/epiUKB/Airwave/scripts/genotype_QC_lite/aw_params.sh
 source $PARAMS
 CMDPATH=$(dirname $PARAMS)/
+
+######################
+### BATCH QC & MERGE #
+######################
 
 ### per-batch preprocessing and QC
 cd ${CMDPATH}
@@ -36,7 +41,8 @@ Rscript ${PBS_O_WORKDIR}/plot-IBD_modified.R all.shared-snps
 ### update HapMap data to the same genome build
 mkdir -p hapmap
 HAPMAPBFILENEW=${PLINKPATH}/hapmap/$(basename $HAPMAPBFILE).${GENOMEBUILD}
-plink --noweb --bfile ${HAPMAPBFILE} --update-alleles ${STRANDPATH}.update_alleles.txt --make-bed --out ${HAPMAPBFILENEW}
+plink --bfile ${HAPMAPBFILE} --update-alleles ${STRANDPATH}.update_alleles.txt \
+      --make-bed --out ${HAPMAPBFILENEW}
 ${PBS_O_WORKDIR}/update_build.sh ${HAPMAPBFILE} ${STRANDPATH}-${GENOMEBUILD}.strand ${HAPMAPBFILENEW}
 
 ### merge with HapMap data
@@ -50,14 +56,18 @@ plink --bfile all.hapmap_snps --bmerge ${HAPMAPBFILENEW}.flipped --extract all.s
 Rscript ${PBS_O_WORKDIR}/aw_ancestry.r
 
 ### remove individuals not passing dataset-wide QC
+cat fail-* | sort -k1 | uniq | cut -f1,2 -d' '> fail-qc-inds.txt
+plink --bfile all.shared-snps --remove fail-qc-inds.txt --make-bed --out all.shared-snps.clean-inds
 
 ######################
 ### QC MARKERS #######
 ######################
 
+### check missing rate
+# markers with missing rates higher than the per-batch threshold are unlikely
+plink --bfile all.shared-snps.clean-inds -missing --out all.shared-snps.clean-inds
+Rscript ${CMDPATH}/lmiss-hist_modified.R all.shared-snps.clean-inds
+
 ### remove markers not passing dataset-wide QC
-plink --bfile XX --maf 0.01 --hwe 0.00001 --out XX
-plink --bfile XX --missing --out XX
-Rscript lmiss-hist.Rscript
-plink --bfile XX --exclude XX --maf 0.01 --hwe 0.00001 --geno 0.05 --make-bed --out XX
+plink --bfile all.shared-snps.clean-inds --geno 0.05 --maf 0.01 --hwe 0.00001 --make-bed --out all.clean-base
 
